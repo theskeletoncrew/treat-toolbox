967cdc5a4967f1039c74dd18e8e37f7480ed14e7
jon wong <j@jnwng.com>
2021-11-29 14:55:32 -0500

    Add another link

--- pages/projects/[projectId]/collections/[collectionId]/traits/[traitId]/index.tsx
+++ pages/projects/[projectId]/collections/[collectionId]/traits/[traitId]/index.tsx
@@ -279,6 +279,32 @@ export default function IndexPage(props: Props) {
                 </Link>
               </span>
 
+              <span className="pr-4">
+                <Link
+                  href={
+                    "/projects/" +
+                    project.id +
+                    "/collections/" +
+                    collection.id +
+                    "/traits/" +
+                    trait.id +
+                    "/values/import-list"
+                  }
+                  passHref={true}
+                >
+                  <button
+                    type="button"
+                    className="inline-flex items-center px-3 py-1 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  >
+                    <DocumentAddIcon
+                      className="-ml-1 mr-1 h-5 w-5"
+                      aria-hidden="true"
+                    />
+                    Import a List of Values
+                  </button>
+                </Link>
+              </span>
+
               <span>
                 <button
                   type="button"
