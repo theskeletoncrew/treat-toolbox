0af4554b0e0f93becf36e55e3ce1926ad124490c
jon wong <j@jnwng.com>
2021-11-29 14:49:33 -0500

    Adding the ability to import trait values from a list

--- pages/projects/[projectId]/collections/[collectionId]/traits/[traitId]/index.tsx
+++ pages/projects/[projectId]/collections/[collectionId]/traits/[traitId]/index.tsx
@@ -179,6 +179,34 @@ export default function IndexPage(props: Props) {
                 </button>
               </Link>
             </div>
+
+            <p className="italic text-sm text-center mt-6 mb-6">or</p>
+
+            <div className="w-full text-center">
+              <Link
+                href={
+                  "/projects/" +
+                  project.id +
+                  "/collections/" +
+                  collection.id +
+                  "/traits/" +
+                  trait.id +
+                  "/values/import-list"
+                }
+                passHref={true}
+              >
+                <button
+                  type="button"
+                  className="inline-flex items-center px-3 py-1 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                >
+                  <DocumentAddIcon
+                    className="-ml-1 mr-1 h-5 w-5"
+                    aria-hidden="true"
+                  />
+                  Import a List of Values
+                </button>
+              </Link>
+            </div>
           </main>
         </div>
       </Layout>
