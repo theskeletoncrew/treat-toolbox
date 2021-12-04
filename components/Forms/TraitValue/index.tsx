82b22dab5e5c59e40d45edafe08d86a55c0b3176
The Skeleton Crew <skeletoncrewrip@gmail.com>
2021-12-04 17:28:49 -0500

    Trait Value: rarity not required when always unique, and form cleanup

--- components/Forms/TraitValue/index.tsx
+++ components/Forms/TraitValue/index.tsx
@@ -14,18 +14,6 @@ interface Props {
   trait: Trait;
 }
 
-const schema = yup
-  .object({
-    name: yup.string().trim().required("This field is required"),
-    rarity: yup
-      .number()
-      .typeError("Must be a number between 0 and 1")
-      .positive("Must be a number between 0 and 1")
-      .max(1, "Must be a number between 0 and 1")
-      .required("This field is required"),
-  })
-  .required();
-
 export const TraitValueForm: React.FC<Props> = ({
   isEdit = false,
   traitValue = null,
@@ -33,6 +21,20 @@ export const TraitValueForm: React.FC<Props> = ({
   collectionId,
   trait,
 }) => {
+  const schema = yup
+    .object({
+      name: yup.string().trim().required("This field is required"),
+      rarity: trait.isAlwaysUnique
+        ? yup.number().notRequired()
+        : yup
+            .number()
+            .typeError("Must be a number between 0 and 1")
+            .positive("Must be a number between 0 and 1")
+            .max(1, "Must be a number between 0 and 1")
+            .required("This field is required"),
+    })
+    .required();
+
   const {
     register,
     handleSubmit,
@@ -81,55 +83,53 @@ export const TraitValueForm: React.FC<Props> = ({
 
   return (
     <TTForm handleSubmit={handleSubmit} submit={onSubmit}>
-      <div className="px-4 py-5 bg-white space-y-6 sm:p-6">
-        <div className="shadow sm:rounded-md sm:overflow-hidden">
-          <div className="px-4 py-5 bg-white space-y-6 sm:p-6">
+      <div className="shadow sm:rounded-md sm:overflow-hidden">
+        <div className="px-4 py-5 bg-white space-y-6 sm:p-6">
+          <div>
+            <label
+              htmlFor="name"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Name
+            </label>
+            <input
+              type="text"
+              id="name"
+              {...register("name")}
+              placeholder="Blue"
+              className="mt-1 block w-full shadow-sm sm:text-sm rounded-md"
+            />
+            {errors.name && (
+              <span className=" text-red-800 text-xs">
+                {errors.name.message}
+              </span>
+            )}
+          </div>
+
+          {trait.isAlwaysUnique ? (
+            ""
+          ) : (
             <div>
               <label
-                htmlFor="name"
+                htmlFor="rarity"
                 className="block text-sm font-medium text-gray-700"
               >
-                Name
+                Rarity
               </label>
               <input
                 type="text"
-                id="name"
-                {...register("name")}
-                placeholder="Blue"
-                className="mt-1 block w-full shadow-sm sm:text-sm rounded-md"
+                {...register("rarity")}
+                id="rarity"
+                placeholder="0.5"
+                className="mt-1 w-20 block shadow-sm sm:text-sm rounded-md"
               />
-              {errors.name && (
+              {errors.rarity && (
                 <span className=" text-red-800 text-xs">
-                  {errors.name.message}
+                  {errors.rarity.message}
                 </span>
               )}
             </div>
-
-            {trait.isAlwaysUnique ? (
-              ""
-            ) : (
-              <div>
-                <label
-                  htmlFor="rarity"
-                  className="block text-sm font-medium text-gray-700"
-                >
-                  Rarity
-                </label>
-                <input
-                  type="text"
-                  {...register("rarity")}
-                  id="rarity"
-                  placeholder="0.5"
-                  className="mt-1 w-20 block shadow-sm sm:text-sm rounded-md"
-                />
-                {errors.rarity && (
-                  <span className=" text-red-800 text-xs">
-                    {errors.rarity.message}
-                  </span>
-                )}
-              </div>
-            )}
-          </div>
+          )}
         </div>
       </div>
     </TTForm>
