45c389756514243203d42210ca54bba7b662db86
Joe Connor <joe@authomedia.co.uk>
2021-12-13 22:47:14 +0000

    fix funciton region for firebase

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,6 +2,7 @@ import * as functions from "firebase-functions";
 import * as httpFunctions from "./httpFunctions";
 
 export const api = functions
+  .region('europe-west2')
   .runWith({
     timeoutSeconds: 540,
     memory: "2GB",
